a33a3786f2617fe1f1bc73415ab99f87c04f617f
thequeensheadcafe <thequeensheadcafe@gmail.com>
2025-03-04 21:55:42 +0000

    v2.3

--- sitemap.js
+++ sitemap.js
@@ -1,19 +1,7 @@
 
 export const revalidate = 3600 // one hour
-import { connectToDatabase } from "@/utils/connectMongo";
-
 export default async function sitemap() {
-  
-  const client = await connectToDatabase();
-  const db = client.db("database");
-  let data = await db.collection("lens").find({}).toArray();
 
-  const lens = data.map((item) => ({
-    url: `${process.env.NEXT_WEBSITE_URL}/lens/${item.slug}`,
-    lastModified: item.updated_at || item.created_at,
-    changefreq: "monthly",
-    priority: 0.6,
-  })); 
 
   return [
     {
