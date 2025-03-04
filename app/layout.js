3dff4a831256a117dfb9742e8bcae0c489908c0e
thequeensheadcafe <thequeensheadcafe@gmail.com>
2025-03-04 17:22:59 +0000

    v2

--- app/layout.js
+++ app/layout.js
@@ -19,6 +19,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth">
+      <meta name="google-site-verification" content="R2xJM5cjh9pSQFhr3DC13FZMn88wq3lyWLWyo8cFnEY" />
       <body
         className={`${outfit.className} ${ovo.className} antialiased leading-8 overflow-x-hidden dark:bg-darkTheme dark:text-white`}
       >
